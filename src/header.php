cfa624f8a048365f0faa7524738d3c06d4840d93
Tanguy Lauriou <tlauriou@gmail.com>
2024-01-10 10:42:47 +0100

    Responsivité

---
+++ src/header.php
@@ -0,0 +1,35 @@
+<header>
+ <style>
+    
+</style>
+ <div class="navbar">
+    <div class="menu-button">
+        <button class="menu-button" id="menu-toggle">Menu</button>
+    </div>
+
+        <nav>
+            <ul>
+               
+                 <li class="MenuItem"><a href="definition.php" class="allLinks">C'est quoi le numérique responsable ?</a></li>
+                    <li class="MenuItem">
+                        <a href="commentEtPourquoiAgir.php" class="allLinks" class="deroulant">Comment et pourquoi agir à mon niveau ?</a>
+                        <ul class="sous">
+                            <li><a href="commentEtPourquoiAgir.php#associationAgir" value="association">Association</a></li>
+                            <li><a href="commentEtPourquoiAgir.php#collectiviteAgir" value="tpe">TPE</a></li>
+                            <li><a href="commentEtPourquoiAgir.php#entrepriseAgir" value="collectivite">Collectivité</a></li>
+                        </ul>
+                    
+                    </li>
+                    <li class="MenuItem"><a href="outilsPourSAutoevaluer.php" class="allLinks">Outils pour s'autoévaluer</a></li>
+                    <li class="MenuItem"><a href="comprendreSesResultats.php" class="allLinks">Comprendre ses résultats</a></li>
+                    <li class="MenuItem">
+                        <a href="..\lib\img\logo.png">
+                            <img src="logo.png" alt="Logo de GreenInit" width="50" height="50">
+                        </a>
+                    </li>
+
+                </li>
+            </ul>
+        </nav>
+</div>
+    </header>
---
+++ src/rgpd.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleGlobal.css">
+    <link rel="icon" href="..\lib\img\logo.png" />
+    <link rel="stylesheet" href="header.css">
+    <title>Inscription</title>
+</head>
+<body>
+<script src="menu-toggle.js"></script>
+    <main>
+        <!--RGPD du site d'auto-diagnostic NR-->
+        <h1>RGPD</h1>
+    </main>
+    <footer>
+        <hr>
+        <div id="footerlinks">
+            <a href="rgpd.html" class="allLinks">RGPD</a>
+        </div>
+    </footer>
+</body>
