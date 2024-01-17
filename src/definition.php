c6b3e603c433e41aa1049f71c059ee455858118d
Matéo <blackwolf4728@gmail.com>
2024-01-17 10:43:37 +0100

    Fix definition.php

--- src/definition.php
+++ src/definition.php
@@ -11,35 +11,37 @@
     <title>Definition du green</title>
 </head>
 <body>
-<script src="menu-toggle.js"></script>    
-    <main>
-        <h1>Le numérique responsable quésaco</h1>
+    <script src="menu-toggle.js"></script>    
+        <main>
+            <h1>Le numérique responsable quésaco</h1>
             <section>
-                    <p>
+                <p>
                     Le numérique responsable est une démarche d’amélioration qui vise à réduire l’empreinte écologique et sociale du numérique. 
                     Le Green IT pour réduire l’empreinte environnementale et l’IT for green qui met le numérique au service du développement durable et la conception responsable des services numériques sont des composant du numériques responsable. 
                     Le numérique responsable est un sujet d’importance pour notre avenir, les mœurs changent et les utilisateur.rice.s aussi. De plus en plus de projets Green voient le jour et c’est aujourd’hui au tour de l’informatique de faire sa transition. Il est donc important de pouvoir évaluer et mettre à niveaux nos outils d’aujourd’hui pour qu’ils puissent encore être utilisés demain. 
-                    </p>
-                    <img src="img1.png" alt="Image 1" width="50" height="50">
-                    <img src="img2.png" alt="Image 2" width="50" height="50">
-                    <p>
-                        Le numérique responsable répond donc à 3 piliers :  
-                        Économie : un projet doit être rentable pour réussir à survivre dans le temps. 
-                        Social : L'outil/application/site que l'on produite doit être utilisable par le plus grand nombre, quel que soit la langue utilisée 
-                        les handicaps ou le niveau d'aisance avec la technologie. Environnement : Ce que l'on produit doit avoir le moins d'impact possible 
-                        sur l'environnement que ce soit à la conception ou à l'utilisation/maintenance. Une fois ces points réunis on peut dire que l'on a 
-                        un outil viable. 
-                    </p>
+                </p>
+                <img src="img1.png" alt="Image 1" width="50" height="50">
+                <img src="img2.png" alt="Image 2" width="50" height="50">
+                <p>
+                    Le numérique responsable répond donc à 3 piliers :  
+                    Économie : un projet doit être rentable pour réussir à survivre dans le temps. 
+                    Social : L'outil/application/site que l'on produite doit être utilisable par le plus grand nombre, quel que soit la langue utilisée 
+                    les handicaps ou le niveau d'aisance avec la technologie. Environnement : Ce que l'on produit doit avoir le moins d'impact possible 
+                    sur l'environnement que ce soit à la conception ou à l'utilisation/maintenance. Une fois ces points réunis on peut dire que l'on a 
+                    un outil viable. 
+                </p>
             </section>
+            <h2>L'accessibilité du web</h2>
             <section>
-                <h2>L'accessibilité du web</h2>
                 <p>Ici, lorsqu'on parle d'accessibilité, nous parlons de l'accessibilité du web, c'est à dire de permettre aux utilisateurs ayant un handicap (mauvaise vision, daltonisme, incapacité à utiliser la souris, ...) de pouvoir naviguer sur le site sans aucune contrainte. </p>
+            </section>
         </main>
         <footer>
-            <!--Footer RGPD-->
-            <hr>
-            <div id="footerlinks">
-                <a href="rgpd.php" class="allLinks">RGPD</a>
-            </div>
-    </footer>
-</body>
+                <!--Footer RGPD-->
+                <hr>
+                <div id="footerlinks">
+                    <a href="rgpd.php" class="allLinks">RGPD</a>
+                </div>
+        </footer>
+    </body>
+</html>
