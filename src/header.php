7de4b786e5a41e9cc907f202e880228ee0caa659
Matéo <blackwolf4728@gmail.com>
2024-01-17 10:22:56 +0100

    Nouveau sous-menu

--- src/header.php
+++ src/header.php
@@ -1,38 +1,39 @@
 <header>
- <style>
-    
-</style>
- <div class="navbar">
-    <div class="menu-button">
-        <button class="menu-button" id="menu-toggle">Menu</button>
-    </div>
-
+    <div class="navbar">
+        <div class="menu-button">
+            <button class="menu-button" id="menu-toggle">Menu</button>
+        </div>
         <nav>
             <ul>
-               
-                 <li class="MenuItem"><a href="definition.php" class="allLinks">C'est quoi le numérique responsable ?</a></li>
-                    <li class="MenuItem">
-                        <a href="commentEtPourquoiAgir.php" class="allLinks" class="deroulant">
-                            Comment et pourquoi agir à mon niveau ?
-                            <span class="triangle">▼</span>
-                        </a>
-                        <ul class="sous">
-                            <li><a href="commentEtPourquoiAgir.php#associationAgir" value="association">Association</a></li>
-                            <li><a href="commentEtPourquoiAgir.php#collectiviteAgir" value="tpe">TPE</a></li>
-                            <li><a href="commentEtPourquoiAgir.php#entrepriseAgir" value="collectivite">Collectivité</a></li>
-                        </ul>
-                    
-                    </li>
-                    <li class="MenuItem"><a href="outilsPourSAutoevaluer.php" class="allLinks">Outils pour s'autoévaluer</a></li>
-                    <li class="MenuItem"><a href="comprendreSesResultats.php" class="allLinks">Comprendre ses résultats</a></li>
-                    <li class="MenuItem">
-                        <a href="index.php">
-                            <img src="/img/logo.png" alt="Logo de GreenInit" width="50" height="50">
-                        </a>
-                    </li>
-
+                <li class="MenuItem">
+                    <a href="definition.php" class="allLinks" class="deroulant">
+                        Introduction au numérique responsable
+                        <span class="triangle">▼</span>
+                    </a>
+                    <ul class="sous">
+                        <li><a href="definition.php" value="allLinks">C'est quoi le numérique responsable ?</a></li>
+                        <li><a href="formulaire.php" value="3U">Est-ce que mon projet respecte les 3U</a></li>
+                    </ul>
+                </li>
+                <li class="MenuItem">
+                    <a href="commentEtPourquoiAgir.php" class="allLinks" class="deroulant">
+                        Comment et pourquoi agir à mon niveau ?
+                        <span class="triangle">▼</span>
+                    </a>
+                    <ul class="sous">
+                        <li><a href="commentEtPourquoiAgir.php#associationAgir" value="association">Association</a></li>
+                        <li><a href="commentEtPourquoiAgir.php#collectiviteAgir" value="tpe">TPE</a></li>
+                        <li><a href="commentEtPourquoiAgir.php#entrepriseAgir" value="collectivite">Collectivité</a></li>
+                    </ul>
+                </li>
+                <li class="MenuItem"><a href="outilsPourSAutoevaluer.php" class="allLinks">Outils pour s'autoévaluer</a></li>
+                <li class="MenuItem"><a href="comprendreSesResultats.php" class="allLinks">Comprendre ses résultats</a></li>
+                <li class="MenuItem">
+                    <a href="index.php">
+                        <img src="/img/logo.png" alt="Logo de GreenInit" width="50" height="50">
+                    </a>
                 </li>
             </ul>
         </nav>
-</div>
-    </header>
+    </div>
+</header>
