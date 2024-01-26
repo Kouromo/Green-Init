a75686b101367f8e9d836f14f3d011564c395d79
Matéo <blackwolf4728@gmail.com>
2024-01-26 10:49:43 +0100

    Accessibilité

--- src/footer.php
+++ src/footer.php
@@ -3,6 +3,5 @@
     <hr>
     <div id="footerlinks">
         <a href="rgpd.php" class="allLinks">RGPD</a>
-        <p>🤫🧏</p>
     </div>
 </footer>
--- src/header.php
+++ src/header.php
@@ -1,35 +1,40 @@
+<?php
+    // Récupérez le nom du fichier actuel.
+    $current_page = basename($_SERVER['PHP_SELF']);
+?>
+
 <header>
     <div class="navbar">
         <div class="menu-button">
-            <button class="menu-button" id="menu-toggle">Menu</button>
+            <button class="menu-button" id="menu-toggle" aria-label="Menu">Menu</button>
         </div>
-        <nav>
+        <nav role="navigation">
             <ul>
                 <li class="MenuItem">
-                    <a href="definition.php" class="allLinks" class="deroulant">
+                    <a href="definition.php" class="allLinks deroulant" aria-haspopup="true" <?php if ($current_page == 'definition.php') echo 'aria-current="page"'; ?>>
                         Introduction au numérique responsable
                         <span class="triangle">▼</span>
                     </a>
                     <ul class="sous">
-                        <li><a href="definition.php" value="allLinks">C'est quoi le numérique responsable ?</a></li>
-                        <li><a href="formulaire.php" value="3U">Est-ce que mon projet respecte les 3U</a></li>
+                        <li><a href="definition.php" value="allLinks" <?php if ($current_page == 'definition.php') echo 'aria-current="page"'; ?>>C'est quoi le numérique responsable ?</a></li>
+                        <li><a href="formulaire.php" value="3U" <?php if ($current_page == 'formulaire.php') echo 'aria-current="page"'; ?>>Est-ce que mon projet respecte les 3U</a></li>
                     </ul>
                 </li>
                 <li class="MenuItem">
-                    <a href="commentEtPourquoiAgir.php" class="allLinks" class="deroulant">
+                    <a href="commentEtPourquoiAgir.php" class="allLinks deroulant" aria-haspopup="true" <?php if ($current_page == 'commentEtPourquoiAgir.php') echo 'aria-current="page"'; ?>>
                         Comment et pourquoi agir à mon niveau ?
                         <span class="triangle">▼</span>
                     </a>
                     <ul class="sous">
-                        <li><a href="commentEtPourquoiAgir.php#associationAgir" value="association">Association</a></li>
-                        <li><a href="commentEtPourquoiAgir.php#collectiviteAgir" value="tpe">TPE</a></li>
-                        <li><a href="commentEtPourquoiAgir.php#entrepriseAgir" value="collectivite">Collectivité</a></li>
+                        <li><a href="commentEtPourquoiAgir.php#associationAgir" value="association" <?php if ($current_page == 'commentEtPourquoiAgir.php') echo 'aria-current="page"'; ?>>Association</a></li>
+                        <li><a href="commentEtPourquoiAgir.php#collectiviteAgir" value="tpe" <?php if ($current_page == 'commentEtPourquoiAgir.php') echo 'aria-current="page"'; ?>>TPE</a></li>
+                        <li><a href="commentEtPourquoiAgir.php#entrepriseAgir" value="collectivite" <?php if ($current_page == 'commentEtPourquoiAgir.php') echo 'aria-current="page"'; ?>>Collectivité</a></li>
                     </ul>
                 </li>
-                <li class="MenuItem"><a href="outilsPourSAutoevaluer.php" class="allLinks">Outils pour s'autoévaluer</a></li>
-                <li class="MenuItem"><a href="comprendreSesResultats.php" class="allLinks">Comprendre ses résultats</a></li>
+                <li class="MenuItem"><a href="outilsPourSAutoevaluer.php" class="allLinks" <?php if ($current_page == 'outilsPourSAutoevaluer.php') echo 'aria-current="page"'; ?>>Outils pour s'autoévaluer</a></li>
+                <li class="MenuItem"><a href="comprendreSesResultats.php" class="allLinks" <?php if ($current_page == 'comprendreSesResultats.php') echo 'aria-current="page"'; ?>>Comprendre ses résultats</a></li>
                 <li class="MenuItem">
-                    <a href="index.php">
+                    <a href="index.php" <?php if ($current_page == 'index.php') echo 'aria-current="page"'; ?>>
                         <img src="/img/logo.png" alt="Logo de GreenInit" width="50" height="50">
                     </a>
                 </li>
