5f66106331ab2f6884a5a18283b68ebb961b58e6
Matéo Alves <92397385+Kouromo@users.noreply.github.com>
2024-03-21 16:25:31 +0100

    Update selectionDOutils.php
    Modification rebase bizarre

--- src/selectionDOutils.php
+++ src/selectionDOutils.php
@@ -1,74 +1,64 @@
 <!DOCTYPE html>
 <html lang="fr">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="styleGlobal.css">
-		<link rel="icon" href="\img\logo.png" />
-		<link rel="stylesheet" href="styleAutoevaluation.css">
-		<link rel="stylesheet" href="header.css">
-		<link rel="stylesheet" href="footer.css">
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
-		<title>Pages d'outils pour s'auto-diagnostiquer</title>
-		<script src="matomo.js"></script>
-	</head>
-	<body>
-		<?php include 'header.php'; ?>
-		<script src="menu-toggle.js"></script>
-		<main>
-			<h1>Notre sélection d'outils pour s'autoévaluer</h1>
-			<form>
-				<article>
-					<h2>Éco-index</h2>
-					<p>
-						Service en ligne proposé par <a href="https://greenit.fr" target="_blank" class="allLinks" aria-label="lien vers le site de Green IT">GreenIT.fr</a>, il fournit une note allant de A à G et une note sur 100, ainsi que quelques conseils pour améliorer son site web. 
-						Quelques mesures sur l'empreinte environnementale sont données pour pouvoir donner à l'utilisateur.rice une idée de la consommation des ressources de sa page. 
-						Le projet est en open source et est amélioré par ses contributeurs. 
-					</p>
-					<a href="https://www.ecoindex.fr/" target="_blank" class="bigLinks" aria-label="lien vers le site éco-index">Éco-index <i class="material-icons-outlined">open_in_new</i></a>
-				</article>
-				<hr>
-				<article>
-					<h2>GTmetrix</h2>
-					<p>
-						Service en ligne permettant de connaitre la vitesse de chargement des pages, grâce à un système de note allant de A à G et de pourcentage d'efficacité permettant 
-						de savoir si le site est performant ainsi que de donner quelques pistes d'amélioration pour améliorer les performances qui sont fortement liée aux critère NR.
-					</p>
-					<a href="https://gtmetrix.com/" target="_blank" class="bigLinks" aria-label="lien vers le site GTmetrix">GTmetrix <i class="material-icons-outlined">open_in_new</i></a>
-				</article>
-				<hr>
-				<article>
-					<h2>Wave</h2>
-					<p>
-						WAVE® est une suite d'outils d'évaluation qui aide les site Web plus accessible aux personnes handicapées. WAVE peut identifier de nombreuses erreurs d'accessibilité 
-						et des lignes directrices pour l'accessibilité du contenu, mais facilite également l'évaluation humaine du contenu Web.
-					</p>
-					<a href="https://wave.webaim.org/" target="_blank" class="bigLinks" aria-label="lien vers le site Wave">Wave <i class="material-icons-outlined">open_in_new</i></a>
-				</article>
-				<hr>
-				<article>
-					<h2>EcoDiag</h2>
-					<p>
-						Ici, nous nous éloignons de votre site web pour mesurer le bilan carbonne de votre structure. Ecodiag permet, à partir d'un inventaire des équipements informatiques 
-						sur un périmètre donné, de calculer le bilan carbone d'une année. 
-					</p>
-					<a href="https://ecoinfo.cnrs.fr/ecodiag-calcul/" target="_blank" class="bigLinks" aria-label="lien vers l'outil Ecodiag sur le site EcoInfo">Ecodiag <i class="material-icons-outlined">open_in_new</i></a>
-				</article>
-				<hr>
-				<article>
-					<h2>Green Web Foundation</h2>
-					<p>
-						 Encore un outil en anglais, celui-ci indique si l'hébergement du site est Green. Si c'est le cas, il fourni un petit sticker au format image montrant que votre site
-						 est hébergé Green, à intégrer à votre site.
-					</p>
-					<a href="https://www.thegreenwebfoundation.org/" target="_blank" class="bigLinks" aria-label="lien vers la page de scan de Green Web Foundation">Green Web Foundation <i class="material-icons-outlined">open_in_new</i></a>
-				</article>
-				<hr>
-				<div class="bouton-result">
-					<a href="notreOutilGreeninit.php"><input class="bouton-vert" type="button" value="Tester son site"/></a>
-				</div>
-			</form>
-		</main>
-		<?php include 'footer.php'; ?>
-	</body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="styleGlobal.css">
+    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="ajouter_retour_exp.css">
+    <link rel="icon" href="\img\logo.png" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,-25" />
+    <script src="https://cdn.tiny.cloud/1/rkuvwlj7ckswfkwb5pee3gh9p7anwlxhdkbrkhbg5mqrvyhc/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <script src="menu-toggle.js"></script>
+    <main>
+        <?php
+        session_start();
+        if (isset ($_SESSION['utilisateur_connecte'])) {
+            echo '<h2>Ajouter un article</h2>';
+            echo '<form action="traitement_retour_exp.php" method="post" enctype="multipart/form-data">';
+                echo '<div>';
+                    echo '<label for="titre">Titre du retour d\'expérience* : </label>';
+                    echo '<input type="text" id="titre" name="titre" required>';
+                echo '</div>';
+                echo '<div>';
+                    echo '<label for="contenu">Contenu du retour d\'expérience* : </label>';
+                    echo '<textarea id="contenu" name="contenu" rows="4" style="width: 100%; height: 192px;" required></textarea>';
+                echo '</div>';
+                echo '<div>';
+                    echo '<label for="media_url"><i class="material-symbols-outlined">attach_file</i> Image associée</label>';
+                    echo '<input type="file" id="media_url" name="media_url" accept="image/*">';
+                    echo '<span id="file-name"></span>';
+                echo '</div>';
+                echo '<div>';
+                    echo '<label for="categorie">Catégorie :</label>';
+                    echo '<select id="categorie" name="categorie">';
+                        echo '<option value="TPE">TPE</option>';
+                        echo '<option value="Association">Association</option>';
+                        echo '<option value="Collectivité">Collectivité</option>';
+                    echo '</select>';
+                echo '</div>';
+                echo '<p>*Champs obligatoires</p>';
+                echo '<input type="submit" class="bouton-vert"value="Ajouter le retour d\'expérience">';
+            echo '</form>';
+            
+        } else {
+            echo '<h2>Erreur : Vous devez vous connecter pour accéder à cette page</h2>';
+            exit();
+        }
+        ?>
+    </main>
+    <script>
+        document.getElementById('media_url').addEventListener('change', function(e) {
+            var fileName = e.target.files[0].name;
+            document.getElementById('file-name').textContent = "Fichier chargé : " + fileName;
+        });
+    </script>
+    <?php include 'footer.php'; ?>
+</body>
+
 </html>
