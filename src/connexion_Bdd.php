bffadfec52d666d2e64b66276591f41c1adc2b96
Matéo <blackwolf4728@gmail.com>
2024-03-28 15:30:10 +0100

    Move to greeninit.antic-paysbasque.com

--- src/connexion_Bdd.php
+++ src/connexion_Bdd.php
@@ -1,19 +1,18 @@
 <?php
 try {
-    $host = "mysql-kouromo.alwaysdata.net"; // Adresse du serveur de la base de données
-    $dbname = "kouromo_greeninit_retourexp"; // Nom de la base de données
-    $user = "kouromo"; // Nom d'utilisateur de la base de données
-    $password = "s'il-vous-plait-aidez-moi"; // Mot de passe de la base de données
+    $host = "paysbasqw2012.mysql.db";
+    $dbname = "paysbasqw2012";
+    $user = "paysbasqw2012";
+    $password = "2hB1V5NT";
 
-    // Options de connexion PDO
+
     $options = array(
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Générer des exceptions en cas d'erreur
-        PDO::ATTR_EMULATE_PREPARES => false, // Désactiver l'émulation des requêtes préparées
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false,
     );
 
-    // Créez une instance PDO pour la connexion à la base de données
-    $connexion = new PDO("mysql:host=$host;dbname=$dbname", $user, $password, $options);
+
+    $connexion = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password, $options);
 } catch (PDOException $e) {
-    // En cas d'erreur de connexion
     die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
--- src/matomo.js
+++ src/matomo.js
@@ -1,15 +1,13 @@
-// Import matomo
-
-// <!-- Matomo -->
-var _paq = window._paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-    var u="//kouromo.alwaysdata.net/matomo/";
+//<!-- Matomo -->
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//greeninit.antic-paysbasque.com/matomo/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-})();
-// <!-- End Matomo Code -->
+  })();
+//<!-- End Matomo Code -->
